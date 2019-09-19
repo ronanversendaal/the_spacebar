d685ec0c0c07d786627c3806d85d28a426037f60
DDEV-Local User <nobody@example.com>
2019-09-19 12:13:49 +0000

    Use a better way of creating lazy loaded twig extensions

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,43 +2,16 @@
 
 namespace App\Twig;
 
-use App\Service\MarkdownHelper;
-use Psr\Container\ContainerInterface;
-use Symfony\Contracts\Service\ServiceSubscriberInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
-class AppExtension extends AbstractExtension implements ServiceSubscriberInterface
+class AppExtension extends AbstractExtension
 {
-
-    /** @var ContainerInterface */
-    private $container;
-
-    public function __construct(ContainerInterface $container)
-    {
-        $this->container = $container;
-    }
-
-    public static function getSubscribedServices()
-    {
-        return [
-            MarkdownHelper::class
-        ];
-    }
-
     public function getFilters(): array
     {
         return [
-            // If your filter generates SAFE HTML, you should add a third
-            // parameter: ['is_safe' => ['html']]
-            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
-            new TwigFilter('cached_markdown', [$this, 'processMarkdown'], ['is_safe' => ['html']]),
+            new TwigFilter('cached_markdown', [AppRuntime::class, 'processMarkdown'], ['is_safe' => ['html']]),
         ];
     }
 
-    public function processMarkdown($value)
-    {
-        return $this->container->get(MarkdownHelper::class)
-            ->parse($value);
-    }
 }
